32295fcfa809c0ab4b4f1623da81d167fb4a0e3d
Danila <danilamatush72@gmail.com>
2025-10-19 23:10:47 +0300

    feat(applciation): add IAuthorService

--- LibraryManagmentSystem.Application/Abstractions/Services/IAuthorService.cs
+++ LibraryManagmentSystem.Application/Abstractions/Services/IAuthorService.cs
@@ -1,6 +1,12 @@
+using LibraryManagmentSystem.Domain.Entities;
+
 namespace LibraryManagmentSystem.Application.Abstractions.Services;
 
 public interface IAuthorService
 {
-    
+    public Task<IEnumerable<Author>> GetAllAsync();
+    public Task<Author?> GetByIdAsync(int id);
+    public Task<Author> CreateAsync(Author author);
+    public Task<Author?> UpdateAsync(int id, Author author);
+    public Task<bool> DeleteAsync(int id);
 }
