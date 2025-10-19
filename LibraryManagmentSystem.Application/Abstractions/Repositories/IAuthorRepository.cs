1291d44d51dddf9813b8a5fbfba1f9eeb04a907b
Danila <danilamatush72@gmail.com>
2025-10-19 22:25:29 +0300

    feat(application): change returned values

--- LibraryManagmentSystem.Application/Abstractions/Repositories/IAuthorRepository.cs
+++ LibraryManagmentSystem.Application/Abstractions/Repositories/IAuthorRepository.cs
@@ -5,8 +5,8 @@ namespace LibraryManagmentSystem.Application.Abstractions.Repositories;
 public interface IAuthorRepository
 {
     public Task<IEnumerable<Author>> GetAllAsync();
-    public Task<Author> GetByIdAsync(int id);
+    public Task<Author?> GetByIdAsync(int id);
     public Task<Author> CreateAsync(Author author);
-    public Task<Author> UpdateAsync(int id, Author author);
-    public Task DeleteAsync(int id);
+    public Task<Author?> UpdateAsync(int id, Author author);
+    public Task<bool> DeleteAsync(int id);
 }
