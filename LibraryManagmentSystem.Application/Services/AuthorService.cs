1288ebb59892b8bfe1fd86c7755a39f788200a42
Danila <danilamatush72@gmail.com>
2025-10-19 23:11:22 +0300

    feat(application): implement IAuthorService in AuthorService

--- LibraryManagmentSystem.Application/Services/AuthorService.cs
+++ LibraryManagmentSystem.Application/Services/AuthorService.cs
@@ -1,6 +1,49 @@
+using LibraryManagmentSystem.Application.Abstractions.Repositories;
+using LibraryManagmentSystem.Application.Abstractions.Services;
+using LibraryManagmentSystem.Domain.Entities;
+
 namespace LibraryManagmentSystem.Application.Services;
 
-public class AuthorService
+public class AuthorService : IAuthorService
 {
+    private readonly IAuthorRepository _authorRepository;
+
+    public AuthorService(IAuthorRepository authorRepository)
+    {
+        _authorRepository = authorRepository;
+    }
     
+    public async Task<IEnumerable<Author>> GetAllAsync()
+    {
+        return await _authorRepository.GetAllAsync();
+    }
+
+    public async Task<Author?> GetByIdAsync(int id)
+    {
+        var author = await _authorRepository.GetByIdAsync(id);
+        if(author == null)
+            throw new KeyNotFoundException($"Author with id {id} not found");
+        return author;
+    }
+
+    public Task<Author> CreateAsync(Author author)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Author?> UpdateAsync(int id, Author author)
+    {
+        var newAuthor = await _authorRepository.UpdateAsync(id, author);
+        if(newAuthor == null)
+            throw new KeyNotFoundException($"Author with id {id} not found");
+        return newAuthor;
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        var success = await _authorRepository.DeleteAsync(id);
+        if(!success)
+            throw new KeyNotFoundException($"Author with id {id} not found");
+        return true;
+    }
 }
