5cfebfb00f5e715c036cd6b01fc28fa936f61c69
Danila <danilamatush72@gmail.com>
2025-10-19 20:58:37 +0300

    feat(domain): add Author model

--- LibraryManagmentSystem.Domain/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace LibraryManagmentSystem.Domain;
-
-public class Class1
-{
-
-}
---
+++ LibraryManagmentSystem.Domain/Entities/Author.cs
@@ -0,0 +1,18 @@
+namespace LibraryManagmentSystem.Domain.Entities;
+
+public class Author
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+
+    public Author(string name, string birthOfDate)
+    {
+        if(string.IsNullOrEmpty(name))
+            throw new ArgumentException("Name cannot be null or empty");
+        if(string.IsNullOrEmpty(birthOfDate))
+            throw new ArgumentException("Birth of date cannot be null or empty");
+        Name = name;
+        DateOfBirth = DateOnly.Parse(birthOfDate);
+    }
+}
