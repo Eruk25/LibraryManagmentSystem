c962d0f23ac234e4f4dcde8b4067df34871e0e32
Danila <danilamatush72@gmail.com>
2025-10-19 21:36:24 +0300

    feat(infrastructure): add temporary lists Books and Authors

--- LibraryManagmentSystem.Infrastructure/Persistance/DB/ApplicationContext.cs
+++ LibraryManagmentSystem.Infrastructure/Persistance/DB/ApplicationContext.cs
@@ -1,6 +1,9 @@
+using LibraryManagmentSystem.Domain.Entities;
+
 namespace LibraryManagmentSystem.Infrastructure.Persistance.DB;
 
 public class ApplicationContext
 {
-    
+    public List<Book> Books { get; set; } = new List<Book>();
+    public List<Author> Authors { get; set; } = new List<Author>();
 }
