0c8fc59695dc6ddbed7072b04f39a0c99ed14916
Danila <danilamatush72@gmail.com>
2025-10-22 22:54:34 +0300

    refactor(application): change returned type in CreateAsync method

--- LibraryManagmentSystem.Application/Abstractions/Repositories/IBookRepository.cs
+++ LibraryManagmentSystem.Application/Abstractions/Repositories/IBookRepository.cs
@@ -6,7 +6,7 @@ public interface IBookRepository
 {
     public Task<IEnumerable<Book>> GetAllAsync();
     public Task<Book?> GetByIdAsync(int id);
-    public Task<Book> CreateAsync(Book book);
+    public Task<int> CreateAsync(Book book);
     public Task<Book?> UpdateAsync(int id, Book book);
     public Task<bool> DeleteAsync(int id);
 }
