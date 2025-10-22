3d1fa91a0beea630b748d6c840461d9ace586d2b
Danila <danilamatush72@gmail.com>
2025-10-22 12:20:48 +0300

    refactor(presentation): move CreateAuthorRequestDto to DTOs/Author/Request

--- LibraryManagmentSystem.API/DTOs/Author/Request/CreateAuthorRequestDto.cs
+++ LibraryManagmentSystem.API/DTOs/Author/Request/CreateAuthorRequestDto.cs
@@ -1,6 +1,6 @@
 namespace LibraryManagmentSystem.API.DTOs;
 
-public class CreateAuthorDto
+public class CreateAuthorRequestDto
 {
     public string Name { get; set; } = string.Empty;
     public string DateOfBirth { get; set; } = string.Empty;
