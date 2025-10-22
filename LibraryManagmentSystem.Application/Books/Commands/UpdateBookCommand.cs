41449edb7cb0d8ebb911afd998d73d1c1facb19d
Danila <danilamatush72@gmail.com>
2025-10-22 23:08:34 +0300

    refactor(application): add Book returned value in UpdateBookCommand

--- LibraryManagmentSystem.Application/Books/Commands/UpdateBookCommand.cs
+++ LibraryManagmentSystem.Application/Books/Commands/UpdateBookCommand.cs
@@ -1,5 +1,6 @@
+using LibraryManagmentSystem.Domain.Entities;
 using MediatR;
 
 namespace LibraryManagmentSystem.Application.Books.Commands;
 
-public record UpdateBookCommand(int Id, string Title, int PublishedYear, int AuthorId) : IRequest;
+public record UpdateBookCommand(int Id, string Title, int PublishedYear, int AuthorId) : IRequest<Book>;
