9b2b19687713cbf90baf69ef13329c998c6db217
Danila <danilamatush72@gmail.com>
2025-10-22 13:46:57 +0300

    refactor(infrastructure): change method of creating lists in applicationContext

--- LibraryManagmentSystem.Infrastructure/Persistance/DB/ApplicationContext.cs
+++ LibraryManagmentSystem.Infrastructure/Persistance/DB/ApplicationContext.cs
@@ -4,6 +4,13 @@ namespace LibraryManagmentSystem.Infrastructure.Persistance.DB;
 
 public class ApplicationContext
 {
-    public List<Book> Books { get; set; } = new List<Book>();
-    public List<Author> Authors { get; set; } = new List<Author>();
+    public List<Author> Authors { get; set; } 
+
+    public List<Book> Books { get; set; }
+
+    public ApplicationContext()
+    {
+        Authors = new List<Author>();
+        Books = new List<Book>();
+    }
 }
