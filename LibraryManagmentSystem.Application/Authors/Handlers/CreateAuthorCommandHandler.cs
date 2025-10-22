4c14c7ea94a5da1d0198a5887c7e6e5505737fb2
Danila <danilamatush72@gmail.com>
2025-10-22 22:32:03 +0300

    feat(application): add CreateAuthorCommandHandler

--- LibraryManagmentSystem.Application/Authors/Handlers/CreateAuthorCommandHandler.cs
+++ LibraryManagmentSystem.Application/Authors/Handlers/CreateAuthorCommandHandler.cs
@@ -1,6 +1,24 @@
+using LibraryManagmentSystem.Application.Abstractions.Repositories;
+using LibraryManagmentSystem.Application.Authors.Commands;
+using LibraryManagmentSystem.Domain.Entities;
+using MediatR;
+
 namespace LibraryManagmentSystem.Application.Authors.Handlers;
 
-public class CreateAuthorCommandHandler
+public class CreateAuthorCommandHandler : IRequestHandler<CreateAuthorCommand, int>
 {
-    
+    private readonly IAuthorRepository _repository;
+
+    public CreateAuthorCommandHandler(IAuthorRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<int> Handle(CreateAuthorCommand request, CancellationToken cancellationToken)
+    {
+        if(request == null)
+            throw new ArgumentNullException(nameof(request));
+        var author = new Author(request.Name, request.DateOfBirth);
+        return await _repository.CreateAsync(author);
+    }
 }
