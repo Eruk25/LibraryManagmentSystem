38faa9ee0c50daacef9e0ecfe685e1aae49a4215
Danila <danilamatush72@gmail.com>
2025-10-22 11:35:50 +0300

    feat(presentation): add CreateAuthorDto

---
+++ LibraryManagmentSystem.API/DTOs/CreateAuthorDto.cs
@@ -0,0 +1,7 @@
+namespace LibraryManagmentSystem.API.DTOs;
+
+public class CreateAuthorDto
+{
+    public string Name { get; set; } = string.Empty;
+    public string DateOfBirth { get; set; } = string.Empty;
+}
