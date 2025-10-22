a9a9eb06e17a0170fb09f9cad72f7ff4007e788b
Danila <danilamatush72@gmail.com>
2025-10-22 12:27:49 +0300

    feat(presentation): register AuthorRepository in DI

--- LibraryManagmentSystem.API/Program.cs
+++ LibraryManagmentSystem.API/Program.cs
@@ -1,9 +1,12 @@
+using LibraryManagmentSystem.Application.Abstractions.Repositories;
+using LibraryManagmentSystem.Infrastructure.Persistance.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
