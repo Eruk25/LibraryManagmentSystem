b76f4267021ec8368761df8b7081cdb3000c7d80
Danila <danilamatush72@gmail.com>
2025-10-22 22:35:31 +0300

    feat(application): add GetByIdAuthorQueryHandler

--- LibraryManagmentSystem.Application/Authors/Handlers/GetByIdAuthorQueryHandler.cs
+++ LibraryManagmentSystem.Application/Authors/Handlers/GetByIdAuthorQueryHandler.cs
@@ -1,9 +1,24 @@
+using LibraryManagmentSystem.Application.Abstractions.Repositories;
 using LibraryManagmentSystem.Application.Authors.Queries;
+using LibraryManagmentSystem.Domain.Entities;
 using MediatR;
 
 namespace LibraryManagmentSystem.Application.Authors.Handlers;
 
-public class GetByIdAuthorQueryHandler : IRequestHandler<GetByIdAuthorQuery>
+public class GetByIdAuthorQueryHandler : IRequestHandler<GetByIdAuthorQuery, Author>
 {
-    
+    private readonly IAuthorRepository _repository;
+
+    public GetByIdAuthorQueryHandler(IAuthorRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<Author> Handle(GetByIdAuthorQuery request, CancellationToken cancellationToken)
+    {
+        var author = await _repository.GetByIdAsync(request.AuthorId);
+        if(author == null)
+            throw new KeyNotFoundException($"Author with id {request.AuthorId} not found");
+        return author;
+    }
 }
