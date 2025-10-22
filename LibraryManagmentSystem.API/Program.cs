2f3592a7ab41ec8deedbb98d8caadccd8da90f46
Danila <danilamatush72@gmail.com>
2025-10-22 13:47:54 +0300

    feat(presentation): register ApplicationContext in DI

--- LibraryManagmentSystem.API/Program.cs
+++ LibraryManagmentSystem.API/Program.cs
@@ -1,21 +1,25 @@
 using LibraryManagmentSystem.Application.Abstractions.Repositories;
 using LibraryManagmentSystem.Application.Abstractions.Services;
 using LibraryManagmentSystem.Application.Services;
+using LibraryManagmentSystem.Infrastructure.Persistance.DB;
 using LibraryManagmentSystem.Infrastructure.Persistance.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ApplicationContext>();
 builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
 builder.Services.AddScoped<IBookRepository, BookRepository>();
 builder.Services.AddScoped<IAuthorService, AuthorService>();
 builder.Services.AddScoped<IBookService, BookService>();
+builder.Services.AddControllers();
+var app = builder.Build();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.MapControllers();
 app.Run();
