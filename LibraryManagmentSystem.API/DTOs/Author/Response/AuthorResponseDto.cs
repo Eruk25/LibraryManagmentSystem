cddcccfad7d21bc1a100d657512b127e8a46722a
Danila <danilamatush72@gmail.com>
2025-10-22 12:22:47 +0300

    feat(presentation): add AuthorResponseDto

--- LibraryManagmentSystem.API/DTOs/Author/Response/AuthorResponseDto.cs
+++ LibraryManagmentSystem.API/DTOs/Author/Response/AuthorResponseDto.cs
@@ -2,5 +2,6 @@ namespace LibraryManagmentSystem.API.DTOs.Author.Response;
 
 public class AuthorResponseDto
 {
-    
+    public string Name { get; set; } = string.Empty;
+    public string DateOfBirth { get; set; } = string.Empty;
 }
