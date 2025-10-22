4dc4ec5ebabcd282b152310e0743f2749fd62d0e
Danila <danilamatush72@gmail.com>
2025-10-22 12:28:59 +0300

    feat(presentation): register AuthorService in DI

--- LibraryManagmentSystem.API/Program.cs
+++ LibraryManagmentSystem.API/Program.cs
@@ -1,4 +1,6 @@
 using LibraryManagmentSystem.Application.Abstractions.Repositories;
+using LibraryManagmentSystem.Application.Abstractions.Services;
+using LibraryManagmentSystem.Application.Services;
 using LibraryManagmentSystem.Infrastructure.Persistance.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +10,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
 builder.Services.AddScoped<IBookRepository, BookRepository>();
+builder.Services.AddScoped<IAuthorService, AuthorService>();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
