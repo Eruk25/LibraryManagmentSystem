4750f541917ae507a1c0d65dafb496ffd882e8af
Danila <danilamatush72@gmail.com>
2025-10-22 12:21:52 +0300

    feat(presentation): add UpdateAuthorRequestDto

--- LibraryManagmentSystem.API/DTOs/Author/Request/UpdateAuthorRequestDto.cs
+++ LibraryManagmentSystem.API/DTOs/Author/Request/UpdateAuthorRequestDto.cs
@@ -2,5 +2,6 @@ namespace LibraryManagmentSystem.API.DTOs;
 
 public class UpdateAuthorRequestDto
 {
-    
+    public string Name { get; set; } = string.Empty;
+    public string DateOfBirth { get; set; } = string.Empty;
 }
