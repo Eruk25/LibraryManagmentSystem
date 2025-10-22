a1f4616ed9645da786231c324a93251550119bc9
Danila <danilamatush72@gmail.com>
2025-10-22 22:51:51 +0300

    fix(domain): rename field PublishedDate to PublishedYear

--- LibraryManagmentSystem.Domain/Entities/Book.cs
+++ LibraryManagmentSystem.Domain/Entities/Book.cs
@@ -4,24 +4,24 @@ public class Book
 {
     public int Id { get; set; }
     public string Title { get; set; }
-    public int PublishedDate { get; set; }
+    public int PublishedYear { get; set; }
     public int AuthorId { get; set; }
     public Author? Author { get; set; }
 
-    public Book(string title, int publishedDate, int authorId)
+    public Book(string title, int publishedYear, int authorId)
     {
         if(string.IsNullOrEmpty(title))
             throw new ArgumentException("Title can not be null or empty");
-        if(!IsValidYear(publishedDate))
+        if(!IsValidYear(publishedYear))
             throw new ArgumentException("The published year is incorrect");
         if(authorId < 0)
             throw new ArgumentException("The authorId can not be negative");
         Title = title;
-        PublishedDate = publishedDate;
+        PublishedYear = publishedYear;
         AuthorId = authorId;
     }
 
-    public bool IsValidYear(int year)
+    private bool IsValidYear(int year)
     {
         var currentDate = DateOnly.Parse(DateTime.UtcNow.Year.ToString());
         return year >= 1900 && year <= currentDate.Year;
