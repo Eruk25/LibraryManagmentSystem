e46c823880070237bce6cb66bd62799c8ca66116
Danila <danilamatush72@gmail.com>
2025-10-23 14:14:38 +0300

    refactor(presentation): add Id field to CreateAuthorRequestDto

--- LibraryManagmentSystem.API/DTOs/Author/Request/CreateAuthorRequestDto.cs
+++ LibraryManagmentSystem.API/DTOs/Author/Request/CreateAuthorRequestDto.cs
@@ -1,7 +1,3 @@
 namespace LibraryManagmentSystem.API.DTOs;
 
-public class CreateAuthorRequestDto
-{
-    public string Name { get; set; } = string.Empty;
-    public string DateOfBirth { get; set; } = string.Empty;
-}
+public record CreateAuthorRequestDto(string Name, string DateOfBirth);
