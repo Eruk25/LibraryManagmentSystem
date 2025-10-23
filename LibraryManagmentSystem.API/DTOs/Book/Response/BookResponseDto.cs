8c566469bca3b71303a79e0711756d74ab59c2df
Danila <danilamatush72@gmail.com>
2025-10-23 16:27:36 +0300

    feat(presentation): add BookResponseDto

--- LibraryManagmentSystem.API/DTOs/Book/Response/BookResponseDto.cs
+++ LibraryManagmentSystem.API/DTOs/Book/Response/BookResponseDto.cs
@@ -1,6 +1,3 @@
 namespace LibraryManagmentSystem.API.DTOs.Book.Response;
 
-public class BookResponseDto
-{
-    
-}
+public record BookResponseDto(int Id, string Title, int PublishedYear, int AuthorId);
