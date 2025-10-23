776c37a263712defcc2a48f491b097a307960de4
Danila <danilamatush72@gmail.com>
2025-10-23 14:12:10 +0300

    feat(application): add DeleteAuthorCommandHandler

---
+++ LibraryManagmentSystem.Application/Authors/Handlers/DeleteAuthorCommandHandler.cs
@@ -0,0 +1,23 @@
+using LibraryManagmentSystem.Application.Abstractions.Repositories;
+using LibraryManagmentSystem.Application.Authors.Commands;
+using MediatR;
+
+namespace LibraryManagmentSystem.Application.Authors.Handlers;
+
+public class DeleteAuthorCommandHandler : IRequestHandler<DeleteAuthorCommand, bool>
+{
+    private readonly IAuthorRepository _authorRepository;
+
+    public DeleteAuthorCommandHandler(IAuthorRepository authorRepository)
+    {
+        _authorRepository = authorRepository;
+    }
+
+    public async Task<bool> Handle(DeleteAuthorCommand request, CancellationToken cancellationToken)
+    {
+        var author = await _authorRepository.GetByIdAsync(request.Id);
+        if(author == null)
+            throw new KeyNotFoundException($"Author with id {request.Id} not found");
+        return await _authorRepository.DeleteAsync(request.Id);
+    }
+}
