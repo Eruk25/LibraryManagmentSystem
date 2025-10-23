3aa2f373dc1a8af7d1391571eb0c9c4f30037a19
Danila <danilamatush72@gmail.com>
2025-10-23 16:01:12 +0300

    refactor(applciation): change returned type from Book? to Book in UpdateAsync method

--- LibraryManagmentSystem.Application/Abstractions/Repositories/IBookRepository.cs
+++ LibraryManagmentSystem.Application/Abstractions/Repositories/IBookRepository.cs
@@ -7,6 +7,6 @@ public interface IBookRepository
     public Task<IEnumerable<Book>> GetAllAsync();
     public Task<Book?> GetByIdAsync(int id);
     public Task<int> CreateAsync(Book book);
-    public Task<Book?> UpdateAsync(int id, Book book);
+    public Task<Book> UpdateAsync(int id, Book book);
     public Task<bool> DeleteAsync(int id);
 }
