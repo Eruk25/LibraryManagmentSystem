07168a8adbadfc2c24fcfae6defa009248b21f1c
Danila <danilamatush72@gmail.com>
2025-10-23 20:04:22 +0300

    fix(presentation): add ToString method from value argument

--- LibraryManagmentSystem.API/Attributes/Validation/YearRangeAttribute.cs
+++ LibraryManagmentSystem.API/Attributes/Validation/YearRangeAttribute.cs
@@ -15,7 +15,7 @@ public class YearRangeAttribute : ValidationAttribute
     {
         if(value == null)
             return ValidationResult.Success;
-        if(!int.TryParse((string)value, out var year))
+        if(!int.TryParse(value.ToString(), out var year))
             return new  ValidationResult("Year must be a number.");
         if(year < _minYear ||  year > DateTime.UtcNow.Year)
             return new  ValidationResult($"Year must be between {_minYear} and {DateTime.UtcNow.Year}.");
