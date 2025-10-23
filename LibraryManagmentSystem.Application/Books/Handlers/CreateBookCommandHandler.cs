eb0dc12b4bca8052d401a031f73c09267a1b3062
Danila <danilamatush72@gmail.com>
2025-10-23 20:09:07 +0300

    fix(application): add authorId validation in CreateBookHandler

--- LibraryManagmentSystem.Application/Books/Handlers/CreateBookCommandHandler.cs
+++ LibraryManagmentSystem.Application/Books/Handlers/CreateBookCommandHandler.cs
@@ -7,18 +7,25 @@ namespace LibraryManagmentSystem.Application.Books.Handlers;
 
 public class CreateBookCommandHandler : IRequestHandler<CreateBookCommand, int>
 {
-    private readonly IBookRepository _repository;
+    private readonly IBookRepository _bookRepository;
+    private readonly IAuthorRepository _authorRepository;
 
-    public CreateBookCommandHandler(IBookRepository repository)
+    public CreateBookCommandHandler(IBookRepository bookRepository, IAuthorRepository authorRepository)
     {
-        _repository = repository;
+        _bookRepository = bookRepository;
+        _authorRepository = authorRepository;
     }
 
     public async Task<int> Handle(CreateBookCommand request, CancellationToken cancellationToken)
     {
         if(request == null)
             throw new ArgumentNullException(nameof(request));
+        
+        var author = await _authorRepository.GetByIdAsync(request.AuthorId);
+        if(author == null)
+            throw new KeyNotFoundException($"Author with id {request.AuthorId} not found");
+        
         var book = new Book(request.Title, request.PublishedYear, request.AuthorId);
-        return await _repository.CreateAsync(book);
+        return await _bookRepository.CreateAsync(book);
     }
 }
