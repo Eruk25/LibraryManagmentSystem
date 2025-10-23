ed78aa53e603eee4dab00ac57fa4a15ee952cc2b
Danila <danilamatush72@gmail.com>
2025-10-23 20:09:49 +0300

    fix(application): add authorId validation in UpdateBookCommandHandler

--- LibraryManagmentSystem.Application/Books/Handlers/UpdateBookCommandHandler.cs
+++ LibraryManagmentSystem.Application/Books/Handlers/UpdateBookCommandHandler.cs
@@ -7,25 +7,37 @@ namespace LibraryManagmentSystem.Application.Books.Handlers;
 
 public class UpdateBookCommandHandler : IRequestHandler<UpdateBookCommand, BookDto>
 {
-    private readonly IBookRepository _repository;
+    private readonly IBookRepository _bookRepository;
+    private readonly IAuthorRepository _authorRepository;
 
-    public UpdateBookCommandHandler(IBookRepository repository)
+    public UpdateBookCommandHandler(IBookRepository bookRepository, IAuthorRepository authorRepository)
     {
-        _repository = repository;
+        _bookRepository = bookRepository;
+        _authorRepository = authorRepository;
     }
 
     public async Task<BookDto> Handle(UpdateBookCommand request, CancellationToken cancellationToken)
     {
-        var book = await _repository.GetByIdAsync(request.Id);
+        var book = await _bookRepository.GetByIdAsync(request.Id);
         if(book == null)
             throw new KeyNotFoundException($"Book with id {request.Id} not found");
+        
         if (!string.IsNullOrEmpty(request.Title))
             book.UpdateTitle(request.Title);
-        if(request.PublishedYear > 0)
-            book.UpdatePublishedYear(request.PublishedYear);
-        if(request.AuthorId > 0)
-            book.UpdateAuthor(request.AuthorId);
-        await _repository.UpdateAsync(request.Id, book);
+        
+        if(request.PublishedYear.HasValue)
+            book.UpdatePublishedYear(request.PublishedYear.Value);
+
+        if (request.AuthorId.HasValue)
+        {
+            var author = await _authorRepository.GetByIdAsync(request.AuthorId.Value);
+            if(author == null)
+                throw new KeyNotFoundException($"Author with id {request.AuthorId} not found");
+            
+            book.UpdateAuthor(request.AuthorId.Value);
+        }
+            
+        await _bookRepository.UpdateAsync(request.Id, book);
         return new BookDto(book.Id, book.Title, book.PublishedYear, book.AuthorId);
     }
 }
