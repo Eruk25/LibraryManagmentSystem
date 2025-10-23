2700f67b4261340c2167d1096a6147b622245114
Danila <danilamatush72@gmail.com>
2025-10-23 09:21:09 +0300

    refactor(domain): change model to immutable model

--- LibraryManagmentSystem.Domain/Entities/Book.cs
+++ LibraryManagmentSystem.Domain/Entities/Book.cs
@@ -2,28 +2,38 @@ namespace LibraryManagmentSystem.Domain.Entities;
 
 public class Book
 {
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public int PublishedYear { get; set; }
-    public int AuthorId { get; set; }
-    public Author? Author { get; set; }
+    public int Id { get; private set; }
+    public string Title { get; private set; }
+    public int PublishedYear { get; private set; }
+    public int AuthorId { get; private set; }
+    public Author? Author { get; private set; }
 
     public Book(string title, int publishedYear, int authorId)
     {
+        UpdateTitle(title);
+        UpdatePublishedYear(publishedYear);
+        UpdateAuthor(authorId);
+    }
+    public void UpdateTitle(string title)
+    {
         if(string.IsNullOrEmpty(title))
-            throw new ArgumentException("Title can not be null or empty");
-        if(!IsValidYear(publishedYear))
-            throw new ArgumentException("The published year is incorrect");
-        if(authorId < 0)
-            throw new ArgumentException("The authorId can not be negative");
+            throw new ArgumentException("Title can not be null or empty", nameof(title));
         Title = title;
+    }
+    public void UpdatePublishedYear(int publishedYear)
+    {
+        if(!IsValidYear(publishedYear))
+            throw new ArgumentException("The published year must be between 1500 and current year", nameof(publishedYear));
         PublishedYear = publishedYear;
+    }
+    public void UpdateAuthor(int authorId)
+    {
+        if(authorId <= 0)
+            throw new ArgumentException("The authorId can not be negative", nameof(authorId));
         AuthorId = authorId;
     }
-
-    private bool IsValidYear(int year)
+    private static bool IsValidYear(int year)
     {
-        var currentDate = DateOnly.Parse(DateTime.UtcNow.Year.ToString());
-        return year >= 1900 && year <= currentDate.Year;
+        return year >= 1500 && year <= DateTime.UtcNow.Year;
     }
 }
