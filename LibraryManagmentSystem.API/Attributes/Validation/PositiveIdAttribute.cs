8562d63c1d32919784aeaee7b9a99c5247bf4a70
Danila <danilamatush72@gmail.com>
2025-10-23 19:36:28 +0300

    feat(presentation): add castom attribute PositiveIdAttribute

--- LibraryManagmentSystem.API/Attributes/Validation/PositiveIdAttribute.cs
+++ LibraryManagmentSystem.API/Attributes/Validation/PositiveIdAttribute.cs
@@ -1,6 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LibraryManagmentSystem.API.Attributes.Validation;
 
-public class PositiveIdAttribute
+public class PositiveIdAttribute : ValidationAttribute
 {
-    
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if(value == null)
+            return ValidationResult.Success;
+        if (!int.TryParse(value.ToString(), out int id))
+            return new ValidationResult("Year must be a number.");
+        if(id <= 0)
+            return new ValidationResult("Year must be greater than zero.");
+        return ValidationResult.Success;
+    }
 }
