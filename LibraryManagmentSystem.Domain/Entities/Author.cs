dd39f1eb314d348e1844bec033a203dfa4b8d5e2
Danila <danilamatush72@gmail.com>
2025-10-23 09:24:32 +0300

    refactor(domain): change Author model to immutable model

--- LibraryManagmentSystem.Domain/Entities/Author.cs
+++ LibraryManagmentSystem.Domain/Entities/Author.cs
@@ -8,13 +8,23 @@ public class Author
 
     public Author(string name, DateOnly dateOfBirth)
     {
+        UpdateName(name);
+        UpdateDateOfBirth(dateOfBirth);
+    }
+
+    public void UpdateName(string name)
+    {
         if(string.IsNullOrEmpty(name))
-            throw new ArgumentException("Name cannot be null or empty");
-        if(DateOfBirth > DateOnly.FromDateTime(DateTime.Today))
-            throw new ArgumentException("Date of birth cannot be in the future");
-        if(dateOfBirth.Year < 1800)
-            throw new ArgumentException("Date of birth cannot be less than 1800");
+            throw new ArgumentException("Name cannot be null or empty", nameof(name));
         Name = name;
+    }
+
+    public void UpdateDateOfBirth(DateOnly dateOfBirth)
+    {
+        if(dateOfBirth > DateOnly.FromDateTime(DateTime.Today))
+            throw new ArgumentException("Date of birth cannot be in the future", nameof(dateOfBirth));
+        if(dateOfBirth.Year < 1400)
+            throw new ArgumentException("Date of birth cannot be less than 1400", nameof(dateOfBirth));
         DateOfBirth = dateOfBirth;
     }
     public Author(){}
