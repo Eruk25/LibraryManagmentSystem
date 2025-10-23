c426caf8007ea00aaf3fecf5ca3a69011c05a154
Danila <danilamatush72@gmail.com>
2025-10-23 10:09:05 +0300

    feat(application): add GetByIdBookQueryHandler

--- LibraryManagmentSystem.Application/Books/Handlers/GetByIdBookQueryHandler.cs
+++ LibraryManagmentSystem.Application/Books/Handlers/GetByIdBookQueryHandler.cs
@@ -1,6 +1,25 @@
+using LibraryManagmentSystem.Application.Abstractions.Repositories;
+using LibraryManagmentSystem.Application.Authors.Queries;
+using LibraryManagmentSystem.Application.Books.Queries;
+using LibraryManagmentSystem.Domain.Entities;
+using MediatR;
+
 namespace LibraryManagmentSystem.Application.Books.Handlers;
 
-public class GetByIdBookQueryHandler
+public class GetByIdBookQueryHandler : IRequestHandler<GetByIdBookQuery, Book>
 {
-    
+    private readonly IBookRepository _repository;
+
+    public GetByIdBookQueryHandler(IBookRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<Book> Handle(GetByIdBookQuery request, CancellationToken cancellationToken)
+    {
+        var book = await _repository.GetByIdAsync(request.BookId);
+        if(book == null)
+            throw new KeyNotFoundException($"Book with id {request.BookId} not found");
+        return book;
+    }
 }
