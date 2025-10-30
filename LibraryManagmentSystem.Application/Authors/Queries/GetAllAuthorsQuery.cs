c64b12084120d79214bcd71332e70944122ac8c5
Danila <danilamatush72@gmail.com>
2025-10-30 16:25:56 +0300

    refactor(applciation): add AuthorsFilter argument to GetAllAuthorsQuery

--- LibraryManagmentSystem.Application/Authors/Queries/GetAllAuthorsQuery.cs
+++ LibraryManagmentSystem.Application/Authors/Queries/GetAllAuthorsQuery.cs
@@ -1,6 +1,7 @@
+using LibraryManagmentSystem.Application.Authors.Filters;
 using LibraryManagmentSystem.Domain.Entities;
 using MediatR;
 
 namespace LibraryManagmentSystem.Application.Authors.Queries;
 
-public record GetAllAuthorsQuery() : IRequest<IEnumerable<AuthorDto>>;
+public record GetAllAuthorsQuery(AuthorsFilter Filters) : IRequest<IEnumerable<AuthorDto>>;
