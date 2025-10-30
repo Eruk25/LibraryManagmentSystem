6c5049facd8b75a6db8cb39bfd530d03123086ed
Danila <danilamatush72@gmail.com>
2025-10-30 17:04:57 +0300

    feat(presentation): add validation to DookFilterQueryDto

--- LibraryManagmentSystem.API/DTOs/Book/Filters/BookFilterQueryDto.cs
+++ LibraryManagmentSystem.API/DTOs/Book/Filters/BookFilterQueryDto.cs
@@ -1,5 +1,9 @@
+using LibraryManagmentSystem.API.Attributes.Validation;
+
 namespace LibraryManagmentSystem.API.DTOs.Book.Filters;
 
 public record BookFilterQueryDto(
+    [param: YearRange(1400)]
     int? PublishedYearFrom,
+    [param: YearRange(1400)]
     int? PublishedYearTo);
