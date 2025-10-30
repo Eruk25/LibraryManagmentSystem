e2b4816fdb754c5e17f1d5eabcb5233dd4a21d18
Danila <danilamatush72@gmail.com>
2025-10-30 15:33:14 +0300

    feat(infrastructure): add class BookQueryableExtensions with ApplyFilter extension method

--- LibraryManagmentSystem.Infrastructure/Extenstions/Books/BookQueryableExtentions.cs
+++ LibraryManagmentSystem.Infrastructure/Extenstions/Books/BookQueryableExtentions.cs
@@ -1,6 +1,18 @@
+using LibraryManagmentSystem.Application.Books.Filters;
+using LibraryManagmentSystem.Domain.Entities;
+
 namespace LibraryManagmentSystem.Infrastructure.Extenstions.Books;
 
 public static class BookQueryableExtentions
 {
-    
+    public static IQueryable<Book> ApplyFilter(this IQueryable<Book> query, BooksFilter filters)
+    {
+        if (filters.PublishedYearFrom.HasValue)
+            query = query.Where(b => b.PublishedYear >= filters.PublishedYearFrom);
+        
+        if(filters.PublishedYearTo.HasValue)
+            query = query.Where(b => b.PublishedYear <= filters.PublishedYearTo);
+        
+        return query;
+    }
 }
