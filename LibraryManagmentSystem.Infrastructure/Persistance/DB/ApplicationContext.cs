7681cdfa160fa4d0f2c5fc770197cba79c0bd411
Danila <danilamatush72@gmail.com>
2025-10-30 15:24:53 +0300

    refactor(infrastructure): use ef instead of memory lists

--- LibraryManagmentSystem.Infrastructure/Persistance/DB/ApplicationContext.cs
+++ LibraryManagmentSystem.Infrastructure/Persistance/DB/ApplicationContext.cs
@@ -1,16 +1,74 @@
 using LibraryManagmentSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace LibraryManagmentSystem.Infrastructure.Persistance.DB;
 
-public class ApplicationContext
+public class LibraryContext : DbContext
 {
-    public List<Author> Authors { get; set; } 
+    public DbSet<Author> Authors { get; set; } 
 
-    public List<Book> Books { get; set; }
+    public DbSet<Book> Books { get; set; }
 
-    public ApplicationContext()
+    public LibraryContext(DbContextOptions<LibraryContext> options)
+        : base(options) {}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        Authors = new List<Author>();
-        Books = new List<Book>();
+        modelBuilder.Entity<Author>()
+            .HasData(
+                new
+                {
+                    Id = 1,
+                    Name = "Федор Достоевский",
+                    DateOfBirth = new DateOnly(1821, 11, 11)
+                },
+                new
+                {
+                    Id = 2,
+                    Name = "Лев Толстой",
+                    DateOfBirth = new DateOnly(1828, 09, 09)
+                });
+        modelBuilder.Entity<Book>()
+            .HasData(
+                new
+                {
+                    Id = 1,
+                    Title = "Бедные люди",
+                    PublishedYear = 1846,
+                    AuthorId = 1
+                },
+                new
+                {
+                    Id = 2,
+                    Title = "Юность",
+                    PublishedYear = 1856,
+                    AuthorId = 2
+                });
+        modelBuilder.Entity<Author>()
+            .HasKey(au => au.Id);
+        modelBuilder.Entity<Author>()
+            .Property(au => au.Name)
+            .HasMaxLength(50)
+            .IsRequired();
+        modelBuilder.Entity<Author>()
+            .Property(au => au.DateOfBirth)
+            .IsRequired();
+        
+        modelBuilder.Entity<Book>()
+            .HasKey(b => b.Id);
+        modelBuilder.Entity<Book>()
+            .Property(b => b.Title)
+            .HasMaxLength(30)
+            .IsRequired();
+        modelBuilder.Entity<Book>()
+            .Property(b => b.PublishedYear)
+            .IsRequired();
+        modelBuilder.Entity<Book>()
+            .Property(b => b.AuthorId)
+            .IsRequired();
+        modelBuilder.Entity<Book>()
+            .HasOne(b => b.Author)
+            .WithMany(au => au.Books)
+            .HasForeignKey(b => b.AuthorId);
     }
 }
