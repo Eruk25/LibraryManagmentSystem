643fa4f2c7046f693f91c9e21d5f3adc608b8181
Danila <danilamatush72@gmail.com>
2025-10-30 15:32:48 +0300

    feat(infrastructure): add class AuthorQueryableExtensions with ApplyFilter extension method

--- LibraryManagmentSystem.Infrastructure/Extenstions/Authors/AuthorQueryableExtensions.cs
+++ LibraryManagmentSystem.Infrastructure/Extenstions/Authors/AuthorQueryableExtensions.cs
@@ -1,6 +1,18 @@
+using LibraryManagmentSystem.Application.Authors.Filters;
+using LibraryManagmentSystem.Domain.Entities;
+
 namespace LibraryManagmentSystem.Infrastructure.Extenstions.Authors;
 
 public static class AuthorQueryableExtensions
 {
-    
+    public static IQueryable<Author> ApplyFilter(this IQueryable<Author> query, AuthorsFilter filters)
+    {
+        if (!string.IsNullOrWhiteSpace(filters.AuthorName))
+            query = query.Where(au => au.Name.Contains(filters.AuthorName));
+        
+        if(filters.QuantityBooks.HasValue)
+            query = query.Where(au => au.Books.Count() == filters.QuantityBooks.Value);
+        
+        return query;
+    }
 }
