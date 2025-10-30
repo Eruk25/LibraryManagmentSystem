9ad0bc3d62219139f4db1510ba5ab1414204bc2a
Danila <danilamatush72@gmail.com>
2025-10-30 15:35:48 +0300

    refactor(application): add AuthorsFilter argument to GetAllAsync in IAuthorRepository

--- LibraryManagmentSystem.Application/Abstractions/Repositories/IAuthorRepository.cs
+++ LibraryManagmentSystem.Application/Abstractions/Repositories/IAuthorRepository.cs
@@ -1,10 +1,11 @@
+using LibraryManagmentSystem.Application.Authors.Filters;
 using LibraryManagmentSystem.Domain.Entities;
 
 namespace LibraryManagmentSystem.Application.Abstractions.Repositories;
 
 public interface IAuthorRepository
 {
-    public Task<IEnumerable<Author>> GetAllAsync();
+    public Task<IEnumerable<Author>> GetAllAsync(AuthorsFilter filters);
     public Task<Author?> GetByIdAsync(int id);
     public Task<int> CreateAsync(Author author);
     public Task<Author> UpdateAsync(int id, Author author);
