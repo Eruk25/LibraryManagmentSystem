b62743a46ed338d02049e6976838264085ac3c8e
Danila <danilamatush72@gmail.com>
2025-10-30 16:29:37 +0300

    refactor(application): add BooksFilter argument to GetAllBooksQuery

--- LibraryManagmentSystem.Application/Books/Queries/GetAllBooksQuery.cs
+++ LibraryManagmentSystem.Application/Books/Queries/GetAllBooksQuery.cs
@@ -1,6 +1,7 @@
+using LibraryManagmentSystem.Application.Books.Filters;
 using LibraryManagmentSystem.Domain.Entities;
 using MediatR;
 
 namespace LibraryManagmentSystem.Application.Books.Queries;
 
-public record GetAllBooksQuery() : IRequest<IEnumerable<BookDto>>;
+public record GetAllBooksQuery(BooksFilter Filters) : IRequest<IEnumerable<BookDto>>;
