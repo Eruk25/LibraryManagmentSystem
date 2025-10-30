58e002ec628f0d98d1fb2d0bd95a1bc60293c32f
Danila <danilamatush72@gmail.com>
2025-10-30 15:42:04 +0300

    refactor(application): add BooksFilter argument to GetAllAsync in IBookRepository

--- LibraryManagmentSystem.Application/Abstractions/Repositories/IBookRepository.cs
+++ LibraryManagmentSystem.Application/Abstractions/Repositories/IBookRepository.cs
@@ -1,10 +1,11 @@
+using LibraryManagmentSystem.Application.Books.Filters;
 using LibraryManagmentSystem.Domain.Entities;
 
 namespace LibraryManagmentSystem.Application.Abstractions.Repositories;
 
 public interface IBookRepository
 {
-    public Task<IEnumerable<Book>> GetAllAsync();
+    public Task<IEnumerable<Book>> GetAllAsync(BooksFilter filter);
     public Task<Book?> GetByIdAsync(int id);
     public Task<int> CreateAsync(Book book);
     public Task<Book> UpdateAsync(int id, Book book);
